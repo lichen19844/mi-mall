4bc6e7e577be2617b683ee802e4b9fc58d921ece
lichen <119449234@qq.com>
2020-05-06 03:33:10 +0800

    set axios

--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
 import Vue from 'vue'
 import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import App from './App.vue'
 
+Vue.use(VueAxios, axios);
 Vue.config.productionTip = false
+
+axios.defaults.baseURL = '/api';
+axios.defaults.timeout = 8000;
+
+// 接口错误拦截
+axios.interceptors.response.use(function(response) {
+  let res = response.data
+  if (res.status === 0) { // axios内部规定的status值
+    return res.data
+  } else if (res.status === 10) {
+    window.location.href = '/#/login'
+  } else {
+    alert(res.msg);
+  }
+})
 
 new Vue({
   router,
