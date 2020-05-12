b3a3856e0cc1c1134bd23061934ebad8ecd7931c
lichen <119449234@qq.com>
2020-05-13 00:40:44 +0800

    pesudo class

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -11,11 +11,21 @@
         <div class="topbar-user">
           <a href="javascript:;">登录</a><span> | </span>
           <a href="javascript:;">注册</a><span> | </span>
-          <a href="javascript:;" class="my-cart"><span class="icon-cart"></span>购物车</a>
+          <a href="javascript:;" class="my-cart"
+            ><span class="icon-cart"></span>购物车</a
+          >
         </div>
       </div>
     </div>
-    <div class="nav-header"></div>
+    <div class="nav-header">
+      <div class="container">
+        <div class="header-logo">
+          <a href="/#/index"></a>
+        </div>
+        <div class="header-menu"></div>
+        <div class="header-search"></div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -26,6 +36,7 @@ export default {
 </script>
 
 <style lang="scss">
+@import './../assets/scss/base.scss';
 .header {
   .nav-topbar {
     height: 39px;
@@ -33,9 +44,6 @@ export default {
     background-color: #333333;
     color: #b0b0b0;
     .container {
-      width: 1226px;
-      margin-right: auto;
-      margin-left: auto;
       display: flex;
       justify-content: space-between;
       align-items: center;
@@ -46,16 +54,58 @@ export default {
       }
       .my-cart {
         width: 110px;
-        background-color: #FF6600;
+        background-color: #ff6600;
         text-align: center;
         color: #ffffff;
         .icon-cart {
           display: inline-block;
           width: 16px;
           height: 12px;
-          background: url('/imgs/icon-cart-checked.png') no-repeat center;
+          background: url("/imgs/icon-cart-checked.png") no-repeat center;
           background-size: contain;
           margin-right: 4px;
+        }
+      }
+    }
+  }
+  .nav-header {
+    .container {
+      height: 112px;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      .header-logo {
+        display: inline-block;
+        width: 55px;
+        height: 55px;
+        background-color: #ff6600;
+        a {
+          display: inline-block;
+          width: 114px;
+          height: 55px;
+          &:before {
+            content: '';
+            display: inline-block;
+            width: 55px;
+            height: 55px;
+            background: url('/imgs/mi-logo.png') no-repeat center;
+            background-size: 55px;
+            transition: margin 0.4s;
+            border: 1px solid green;
+          }
+          &:after {
+            content: '';
+            display: inline-block;
+            width: 55px;
+            height: 55px;
+            background: url('/imgs/mi-home.png') no-repeat center;
+            background-size: 55px;
+            transition: margin 0.4s;
+            border: 1px solid red;
+          }
+          &:hover:before {
+            margin-left: -55px;
+          }
         }
       }
     }
