d8d127f07a7903073ea231a21b8ce5c4121011e9
lichen <119449234@qq.com>
2020-07-19 23:59:03 +0800

    use syntax-dynamic-import

--- src/router.js
+++ src/router.js
@@ -2,10 +2,10 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 import Home from './pages/home'
-import Login from './pages/login'
+// import Login from './pages/login'
 import Index from './pages/index'
-import Product from './pages/product'
-import Detail from './pages/detail'
+// import Product from './pages/product'
+// import Detail from './pages/detail'
 import Cart from './pages/cart'
 import Order from './pages/order'
 import OrderList from './pages/orderList'
@@ -31,19 +31,23 @@ export default new Router({
         {
           path: '/product/:id',
           name: 'product',
-          component: Product
+          // component: Product
+          component: resolve => require(['./pages/index.vue'], resolve),
         },
         {
           path: '/detail/:id',
           name: 'detail',
-          component: Detail
+          // component: Detail
+          component: resolve => require(['./pages/detail.vue'], resolve),
         },
       ]
     },
     {
       path: '/login',
       name: 'login',
-      component: Login,
+      // component: Login,
+      // component: resolve => require(['./pages/login.vue'], resolve),
+      component: () => import('./pages/login.vue'),
     },
     {
       path: '/cart',
@@ -53,7 +57,8 @@ export default new Router({
     {
       path: '/order',
       name: 'order',
-      component: Order,
+      // component: Order,
+      component: () => import('./pages/order.vue'),
       children: [
         {
           path: 'list',
